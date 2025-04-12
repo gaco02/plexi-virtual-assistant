22c2326198513116bb9d0f264185d20d21453f5e
gaco02 <osvaldo.gaco09@gmail.com>
2025-04-11 18:45:51 -0700

    fixed ios working

--- plexi_virtual_assistant/ios/Runner/AppDelegate.swift
+++ plexi_virtual_assistant/ios/Runner/AppDelegate.swift
@@ -1,5 +1,7 @@
 import Flutter
 import UIKit
+import FirebaseCore  // ✅ <-- Make sure this line is present
+
 
 @main
 @objc class AppDelegate: FlutterAppDelegate {
@@ -7,6 +9,7 @@ import UIKit
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    FirebaseApp.configure() // Ensure this line is present
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
